e889e4c2276069c009b9365f9eb4eee1f1d84e39
DaanMemelink <memelinkd@gmail.com>
2024-09-23 14:55:26 +0200

    Retry policy implemented

--- src/ChessGameDDD.ApplicationLayer/CommandHandlers/MakeMoveCommandHandler.cs
+++ src/ChessGameDDD.ApplicationLayer/CommandHandlers/MakeMoveCommandHandler.cs
@@ -1,37 +1,51 @@
-﻿using ChessGameDDD.ApplicationLayer.Commands;
+﻿using System;
+using ChessGameDDD.ApplicationLayer.Commands;
 using ChessGameDDD.Domain.Entities;
 using ChessGameDDD.Events;
 using System.Collections.Generic;
+using Polly;
 
 namespace ChessGameDDD.ApplicationLayer.CommandHandlers
 {
     public class MakeMoveCommandHandler
     {
+        private readonly Policy _retryPolicy;
+        
         public MakeMoveCommandHandler()
         {
+            _retryPolicy = Policy
+                .Handle<Exception>()
+                .Retry(3, (exception, retryCount) =>
+                {
+                    // Log the retry attempt
+                    Console.WriteLine($"Retry {retryCount} due to {exception.Message}");
+                });
         }
 
         public Game HandleCommand(MakeMoveCommand moveCommand)
         {
-            // Create game via eventsourcing from DB
-            // Get old events from DB (infrastructure layer)
-            var oldEvents = new List<Event>();
+            return _retryPolicy.Execute(() =>
+            {
+                // Create game via eventsourcing from DB
+                // Get old events from DB (infrastructure layer)
+                var oldEvents = new List<Event>();
 
-            var game = Game.Create(oldEvents);
+                var game = Game.Create(oldEvents);
 
-            // Map command to move
-            // var move = new Move();
+                // Map command to move
+                // var move = new Move();
 
-            // game.MakeMove(move);
+                // game.MakeMove(move);
 
-            // persist events
-            // Save to database (Infrastructure layer)
-            var eventsToSave = game.GetEvents();
+                // persist events
+                // Save to database (Infrastructure layer)
+                var eventsToSave = game.GetEvents();
 
-            // publsih events
-            // MessagePublisher.publish(game.GetEvents());
+                // publsih events
+                // MessagePublisher.publish(game.GetEvents());
 
-            return game;
+                return game;
+            });   
         }
     }
 }
